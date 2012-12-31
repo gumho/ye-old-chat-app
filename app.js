d6d5818b74cac42e468379a64dc17910d6a1a1e2
Ryan <ryanlives@gmail.com>
2012-12-30 19:31:14 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var io = require('socket.io')
+  , express = require('express')
+  , stylus = require('stylus')
+
+var app = express()
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(stylus.middleware({ 
+  src: __dirname + '/public', 
+  compile: function(str, path) {
+    return stylus(str).set('filename', path);
+  }
+}))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+var server = require('http').createServer(app)
+  , io = io.listen(server);
+
+server.listen(8000);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('register nick', function(nick) {
+    socket.nickname = nick;
+    socket.broadcast.emit('announcement', nick + ' connected');
+  });
+
+  socket.on('user message', function(nick, msg) {
+    socket.broadcast.emit('user message', nick, msg);
+  });
+
+  socket.on('disconnect', function() {
+    if (!socket.nickname) return;
+    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+  })
+});
